6e771df24f1f752f4047bfcbfd078014aa4c6f10
eonuyen <onuryenici@yahoo.com>
2017-07-06 17:30:45 +0300

    Update and rename LoginTest.java to LoginJUnitTest.java

---
+++ LoginJUnitTest.java
@@ -0,0 +1,62 @@
+package selenium.tests;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import selenium.configReaders.*;
+import selenium.framework.ParentFunctionWeb;
+import selenium.helpers.Helper;
+import selenium.tests.pages.LoginPage;
+/**
+ * Created by onur.yenici on 5.06.2017.
+ */
+public class LoginTest extends ParentFunctionWeb {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        openBrowser();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+
+        quitBrowser();
+    }
+
+    @Before
+    public void beforeMethod() throws Exception {
+
+        clearCookiesAndGoHomePage();
+
+    }
+
+    @After
+    public void afterMethod() throws Exception {
+
+        try{
+            Helper.SearchAndFindElement(By.className("js-error-out"),driver).findElements(By.tagName("Button")).get(0).click();
+
+        }catch(Exception e){
+
+        }
+
+    }
+
+    @Test()
+    @Parameters(name="C2980")
+    public void successfullLogin() throws Exception{
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.
+                clickLogin().
+                doLogin(
+                        conf.getOzanWalletWebUserName(),
+                        conf.getOzanWalletWebUserPassword(),
+                        conf.getOzanWalletCountryCode()
+                );
+
+        Helper.clickObjectById("panel-content",driver);
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
+
+    }
+}
